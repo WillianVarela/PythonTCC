15e125a1baaa68519688c5ded9c723675e9114b2
WillNightray <willian-varela@hotmail.com>
2020-10-09 22:10:29 -0300

    Ajustes

--- deeplearning.py
+++ deeplearning.py
@@ -1,12 +1,12 @@
 import tensorflow as tf
 import numpy as np
-from PIL import Image
-#import matplotlib.pyplot as plt
-import os
+import matplotlib.pyplot as plt
 import pathlib
-from tensorflow.keras.models import Sequential, load_model
-from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D
 import base64
+import itertools
+from tensorflow.keras.models import Sequential, load_model
+from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, experimental
+from sklearn.metrics import classification_report, confusion_matrix
 
 BATCH_SIZE = 100 # epocas * steps
 IMG_HEIGHT = 200
@@ -45,70 +45,111 @@ class DeepLearning(object):
         return train_data_gen, valid_data_gen
 
     def training_IA(self):
+        # DataAgumentation, flip das imagens
+        data_augmentation = tf.keras.Sequential([
+            experimental.preprocessing.RandomFlip("horizontal_and_vertical")
+        ])
+
         model = Sequential([
-            Conv2D(64, (2, 2), padding='same', activation='relu',
+            data_augmentation, # Realizar considerações para oque melhorou ou pirou para esta função
+            Conv2D(32, (2, 2), padding='same', activation='relu',
                    input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)), 
             MaxPooling2D((2, 2)),
             Dropout(0.2), 
             Conv2D(32, (3, 3), padding='same', activation='relu'),
             MaxPooling2D((2, 2)),
             Dropout(0.2),
-            Conv2D(64, (3, 3), padding='same', activation='relu'),
+            Conv2D(128, (3, 3), padding='same', activation='relu'),
             MaxPooling2D((2, 2)),
             Dropout(0.2),
             Flatten(),
-            Dense(256, activation='relu'),
+            Dense(256, activation='sigmoid', kernel_initializer='he_normal'),
             Dropout(0.4),
             Dense(2, activation='sigmoid') 
         ])
         model.compile(optimizer='adam', # otimazar adam padrão
                       loss='binary_crossentropy', # cassificação binaria
                       metrics=['accuracy']) # accuracy simples
-        model.summary()
+        # ModelCheckpoint - Salva o modelo com menor loss, ou seja o melhor modelo treinado
+        mdlckpt = tf.keras.callbacks.ModelCheckpoint('model.h5', monitor='val_loss', verbose=1, mode='min', save_best_only=True)
+
+        # EarlyStop -> Se o loss não diminuir para o treinamento, pois a rede não está aprendendo mais (se não diminuir em 4 epocos ele para)
+        earlystop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.001, patience=4, mode='min', verbose=1)
+
         train_data_gen, valid_data_gen = DeepLearning.get_images(self)
         image_train, label_train = next(train_data_gen)
         image_valid, label_valid = next(valid_data_gen)
         history = model.fit(image_train, label_train, epochs=EPOCHS, steps_per_epoch=STEPS_TRAIN,
-                            validation_data=(image_valid, label_valid), validation_steps=STEPS_VAL)
+                            validation_data=(image_valid, label_valid), validation_steps=STEPS_VAL, callbacks=[mdlckpt, earlystop])
+        model.summary()
         model.save("model.h5")
 
-        #acc = history.history['accuracy']
-        #val_acc = history.history['val_accuracy']
-
-        #loss = history.history['loss']
-        #val_loss = history.history['val_loss']
-
-        #epochs_range = range(EPOCHS)
-
-        #plt.figure(figsize=(8, 8))
-        #plt.subplot(1, 2, 1)
-        #plt.plot(epochs_range, acc, label='Training Accuracy')
-        #plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-        #plt.legend(loc='lower right')
-        #plt.title('Training and Validation Accuracy')
-
-        #plt.subplot(1, 2, 2)
-        #plt.plot(epochs_range, loss, label='Training Loss')
-        #plt.plot(epochs_range, val_loss, label='Validation Loss')
-        #plt.legend(loc='upper right')
-        #plt.title('Training and Validation Loss')
-        #plt.show()
+        self.matrix_confusion(model, valid_data_gen)
+        self.plot_result(history)
         pass
+
+    def matrix_confusion(self, model, valid_data_gen):
+        target_names = ['Ferrugem', 'Sadia']
+        cm_title = 'Confusion Matrix'
+        tick_marks = np.arange(2)
+        Y_pred = model.predict(valid_data_gen, STEPS_VAL)
+        y_pred = np.argmax(Y_pred, axis=1)
+        cm = confusion_matrix(valid_data_gen.classes, y_pred)
+        plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
+        plt.title(cm_title)
+        plt.xticks(tick_marks, target_names, rotation=45)
+        plt.yticks(tick_marks, target_names)
+        thresh = cm.max() / 2.
+        for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+            plt.text(j, i, format(cm[i, j], 'd'), horizontalalignment="center",
+                     color="white" if cm[i, j] > thresh else "black")
+
+        plt.ylabel('Label Verdadeiro')
+        plt.xlabel('Predicted Label')
+        plt.tight_layout()
+        print(cm_title)
+        print(cm)
+        print('Classification Report')
+        print(classification_report(valid_data_gen.classes, y_pred, target_names=target_names))
+    pass
+
+    def plot_result(self, history):
+        acc = history.history['accuracy']
+        val_acc = history.history['val_accuracy']
+
+        loss = history.history['loss']
+        val_loss = history.history['val_loss']
+
+        epochs_range = range(EPOCHS)
+
+        plt.figure(figsize=(8, 8))
+        plt.subplot(1, 2, 1)
+        plt.plot(epochs_range, acc, label='Training Accuracy')
+        plt.plot(epochs_range, val_acc, label='Validation Accuracy')
+        plt.legend(loc='lower right')
+        plt.title('Training and Validation Accuracy')
+
+        plt.subplot(1, 2, 2)
+        plt.plot(epochs_range, loss, label='Training Loss')
+        plt.plot(epochs_range, val_loss, label='Validation Loss')
+        plt.legend(loc='upper right')
+        plt.title('Training and Validation Loss')
+        plt.show()
+    pass
 
     def preditc_IA(self, image_request):
         imgdata = base64.b64decode(image_request) # decodifica a image de base64
-        with open("predictimg.jpeg", 'wb') as f: # salva a imagem no diretorio do projeto
+        with open("predictimg.png", 'wb') as f: # salva a imagem no diretorio do projeto
             f.write(imgdata)
-        # Restaura a imagem utilizando keras
-        test_image = tf.keras.preprocessing.image.load_img("predictimg.jpeg", target_size = (200, 200))  
-        test_image = tf.keras.preprocessing.image.img_to_array(test_image) # Covnerte a imagem para um array
+        # Restaura a imagem utilizando keras, normalizar esses dados
+        test_image = tf.keras.preprocessing.image.load_img("predictimg.png", target_size = (200, 200))
+        test_image = tf.keras.preprocessing.image.img_to_array(test_image) # Converte a imagem para um array
         test_image = np.expand_dims(test_image, axis = 0) # expande o array
         model = load_model('model.h5') # restaura o modelo
         predict = model.predict(test_image) # realiza a predição
-        
-        print(CLASS_NAMES[np.argmax(predict[0])], predict)
-        return CLASS_NAMES[np.argmax(predict[0])]
+        print(CLASS_NAMES[np.argmax(predict[0])], predict[0])
+        return '{"ferrugem": ' + str(predict[0][0]) + ', "sadia": ' + str(predict[0][1]) + '}'
 
-#ia = DeepLearning()
-#ia.training_IA()
-#ia.preditc_IA('ssss')
+# ia = DeepLearning()
+# ia.training_IA()
+# ia.preditc_IA('ssss')
