9ef042a8fb5e155c12d44fabbba78dbb16762900
WillNightray <willian-varela@hotmail.com>
2020-11-07 04:00:16 -0300

    Ajustes

--- deeplearning.py
+++ deeplearning.py
@@ -3,105 +3,95 @@ import numpy as np
 import matplotlib.pyplot as plt
 import itertools
 import pathlib
-import cv2
 import base64
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras.models import Sequential, load_model
 from tensorflow.keras.layers import Dense, Conv2D, Flatten, Dropout, MaxPooling2D, experimental, GaussianNoise
 from sklearn.metrics import classification_report, confusion_matrix
 
-BATCH_SIZE = 128 # epocas * steps
-IMG_HEIGHT = 200
-IMG_WIDTH = 200
-EPOCHS = 10
-STEPS_TRAIN = 10
-STEPS_VAL = 5
+BATCH_SIZE = 128
+IMG_HEIGHT = 64
+IMG_WIDTH = 64
+EPOCHS = 50
 train_dir = pathlib.Path('train/')
-valid_dir = pathlib.Path('validation/')
-CLASS_NAMES = []
+test_dir = pathlib.Path('test/')
 CLASS_NAMES = np.array([item.name for item in train_dir.glob('*')])
+tf.random.set_seed(221)
 
 class DeepLearning(object):
 
     def get_images(self):
-        # Normalizando as imagens
-        image_generator = tf.keras.preprocessing.image.ImageDataGenerator(
-            rescale=1./255,
-        )
-        image_generator2 = tf.keras.preprocessing.image.ImageDataGenerator(
-            rescale=1./255,
-        )
         # Carregando as imagens do diretorio, passando o batch e tamanho assim como a classe
-        train_data_gen = image_generator.flow_from_directory(directory=str(train_dir),
-                                                             batch_size=BATCH_SIZE,
-                                                             target_size=(
-                                                                 IMG_HEIGHT, IMG_WIDTH),
-                                                                 class_mode="categorical"
-                                                             )
-        valid_data_gen = image_generator2.flow_from_directory(directory=str(valid_dir),
+        train_data_gen = ImageDataGenerator().flow_from_directory(directory=str(train_dir),
                                                              batch_size=BATCH_SIZE,
                                                              target_size=(
                                                                  IMG_HEIGHT, IMG_WIDTH),
                                                                  class_mode="categorical"
                                                              )
 
-        return train_data_gen, valid_data_gen
+        teste_data_gen = ImageDataGenerator().flow_from_directory(directory=str(test_dir),
+                                                                  batch_size=BATCH_SIZE,
+                                                                  target_size=(
+                                                                      IMG_HEIGHT, IMG_WIDTH),
+                                                                  class_mode="categorical"
+                                                                  )
+
+        return train_data_gen, teste_data_gen
 
     def training_IA(self):
-        # DataAgumentation, flip das imagens
         data_augmentation = tf.keras.Sequential([
             tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal_and_vertical"),
             tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
-            tf.keras.layers.experimental.preprocessing.RandomContrast(factor=0.1)
         ])
 
         model = Sequential([
-            tf.keras.layers.experimental.preprocessing.Normalization(),
-            data_augmentation, # Realizar considerações para oque melhorou ou pirou para esta função
-            Conv2D(32, (2, 2), padding='same', activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
+            data_augmentation,
+            Conv2D(32, (2, 2),  activation='relu', input_shape=(IMG_HEIGHT, IMG_WIDTH, 3)),
             MaxPooling2D((2, 2)),
-            Dropout(0.2),
-            Conv2D(64, (3, 3), padding='same', activation='relu'),
+            Conv2D(64, (3, 3),  activation='relu'),
             MaxPooling2D((2, 2)),
-            Dropout(0.2),
-            Conv2D(128, (3, 3), padding='same', activation='relu'),
+            Conv2D(128, (3, 3),  activation='relu'),
             MaxPooling2D((2, 2)),
-            Dropout(0.2),
             Flatten(),
-            Dense(256, activation='sigmoid'),
-            Dropout(0.4),
-            Dense(2, activation='sigmoid')
+            Dense(256, activation='relu', kernel_regularizer='l2'),
+            Dropout(.2),
+            Dense(len(CLASS_NAMES), activation='softmax', kernel_regularizer='l2')
         ])
-        model.compile(optimizer='adam', # otimazar adam padrão
-                      loss='binary_crossentropy', # cassificação binaria
-                      metrics=['accuracy']) # accuracy simples
-        # ModelCheckpoint - Salva o modelo com menor loss, ou seja o melhor modelo treinado
+        model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
         mdlckpt = tf.keras.callbacks.ModelCheckpoint('model_cpk.h5', monitor='val_loss', verbose=1, mode='min',
                                                      save_best_only=True)
 
-        # EarlyStop -> Se o loss não diminuir para o treinamento, pois a rede não está aprendendo mais
-        # (se não diminuir em 4 epocos ele para)
-        earlystop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', min_delta=0.001, patience=4, mode='min',
-                                                     verbose=1)
+        earlystop = tf.keras.callbacks.EarlyStopping(monitor='val_loss', patience=4, mode='min', verbose=1)
 
-        train_data_gen, valid_data_gen = DeepLearning.get_images(self)
+        train_data_gen, test_data = DeepLearning.get_images(self)
         image_train, label_train = next(train_data_gen)
-        image_valid, label_valid = next(valid_data_gen)
-        history = model.fit(image_train, label_train, epochs=EPOCHS, steps_per_epoch=STEPS_TRAIN,
-                            validation_data=(image_valid, label_valid), validation_steps=STEPS_VAL,
+        test_data, teste_valid = next(test_data)
+        image_train = image_train - np.mean(image_train, axis=0)
+        image_train = image_train / np.std(image_train, axis=0)
+        test_data = test_data - np.mean(test_data, axis=0)
+        test_data = test_data / np.std(test_data, axis=0)
+
+        history = model.fit(image_train, label_train, epochs=EPOCHS, validation_split=0.2,
                             callbacks=[mdlckpt, earlystop])
+
+        test_loss, test_acc = model.evaluate(test_data, teste_valid)
+        print(test_acc)
+
         model.summary()
         model.save("model.h5")
 
-        self.matrix_confusion(model, valid_data_gen)
+        self.matrix_confusion(model, test_data, teste_valid)
         self.plot_result(history)
         pass
 
-    def matrix_confusion(self, model, valid_data_gen):
+    def matrix_confusion(self, model, valid_data_gen, image_valid):
         cm_title = 'Confusion Matrix'
-        tick_marks = np.arange(2)
-        Y_pred = model.predict(valid_data_gen, STEPS_VAL)
+        tick_marks = np.arange(len(CLASS_NAMES))
+        Y_pred = model.predict(valid_data_gen)
         y_pred = np.argmax(Y_pred, axis=1)
-        cm = confusion_matrix(valid_data_gen.classes, y_pred)
+        rounded_labels = np.argmax(image_valid, axis=1)
+        cm = confusion_matrix(rounded_labels, y_pred)
         plt.imshow(cm, interpolation='nearest', cmap=plt.cm.Blues)
         plt.title(cm_title)
         plt.xticks(tick_marks, CLASS_NAMES, rotation=45)
@@ -117,7 +107,7 @@ class DeepLearning(object):
         print(cm_title)
         print(cm)
         print('Classification Report')
-        print(classification_report(valid_data_gen.classes, y_pred, target_names=CLASS_NAMES))
+        print(classification_report(rounded_labels, y_pred, target_names=CLASS_NAMES))
     pass
 
     def plot_result(self, history):
@@ -131,16 +121,16 @@ class DeepLearning(object):
 
         plt.figure(figsize=(8, 8))
         plt.subplot(1, 2, 1)
-        plt.plot(epochs_range, acc, label='Training Accuracy')
+        plt.plot(epochs_range, acc, label='Accuracy')
         plt.plot(epochs_range, val_acc, label='Validation Accuracy')
         plt.legend(loc='lower right')
-        plt.title('Training and Validation Accuracy')
+        plt.title('Accuracy')
 
         plt.subplot(1, 2, 2)
-        plt.plot(epochs_range, loss, label='Training Loss')
+        plt.plot(epochs_range, loss, label='Loss')
         plt.plot(epochs_range, val_loss, label='Validation Loss')
         plt.legend(loc='upper right')
-        plt.title('Training and Validation Loss')
+        plt.title('Loss')
         plt.show()
     pass
 
@@ -149,14 +139,70 @@ class DeepLearning(object):
         with open("predictimg.png", 'wb') as f: # salva a imagem no diretorio do projeto
             f.write(imgdata)
         # Restaura a imagem utilizando keras, normalizar esses dados
-        test_image = tf.keras.preprocessing.image.load_img("predictimg.png", target_size = (200, 200))
-        test_image = tf.keras.preprocessing.image.img_to_array(test_image) # Converte a imagem para um array
+        test_image = tf.keras.preprocessing.image.load_img("predictimg.png", target_size = (IMG_HEIGHT, IMG_WIDTH))
+        # test_image = tf.keras.preprocessing.image.img_to_array(test_image) # Converte a imagem para um array
+        test_image = test_image - np.mean(test_image, axis=0)  # zero-centering
+        test_image = test_image / np.std(test_image, axis=0)  # normalization
         test_image = np.expand_dims(test_image, axis = 0) # expande o array
         model = load_model('model.h5') # restaura o modelo
         predict = model.predict(test_image) # realiza a predição
         print(CLASS_NAMES[np.argmax(predict[0])], predict[0])
         return '{"ferrugem": ' + str(predict[0][0]) + ', "sadia": ' + str(predict[0][1]) + '}'
 
-# ia = DeepLearning()
-# ia.training_IA()
-# ia.preditc_IA('ssss')
+    def download_tflite(self):
+        model = tf.keras.models.load_model('model.h5')
+        converter = tf.lite.TFLiteConverter.from_keras_model(model)
+        tflite_model = converter.convert()
+        open("converted_model.tflite", "wb").write(tflite_model)
+        pass
+
+ia = DeepLearning()
+ia.training_IA()
+# ia.download_tflite()
+# test_data = test_data - np.mean(test_data, axis=0)  # zero-centering
+# test_data = test_data / np.std(test_data, axis=0)  # normalization
+# img = cv2.imread('./train/ferrugem/ferrugem_tan_049.png')
+#
+# img_to_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+# img_to_yuv[:, :, 0] = cv2.equalizeHist(img_to_yuv[:, :, 0])
+# hist_equalization_result = cv2.cvtColor(img_to_yuv, cv2.COLOR_YUV2BGR)
+# cv2.imwrite('result.jpg', hist_equalization_result)
+#
+# color = ('b','g','r')
+# for channel,col in enumerate(color):
+#     histr = cv2.calcHist([img],[channel],None,[256],[0,256])
+#     plt.subplot(1, 4, 1)
+#     plt.plot(histr,color = col)
+#     plt.xlim([0,256])
+# plt.title('ferrugem normal')
+#
+# color = ('b','g','r')
+# for channel,col in enumerate(color):
+#     histr = cv2.calcHist([hist_equalization_result],[channel],None,[256],[0,256])
+#     plt.subplot(1, 4, 2)
+#     plt.plot(histr,color = col)
+#     plt.xlim([0,256])
+# plt.title('ferrugme equal')
+#
+# img = cv2.imread('./train/sadia/sadia_091.png')
+# color = ('b','g','r')
+# for channel,col in enumerate(color):
+#     histr = cv2.calcHist([img],[channel],None,[256],[0,256])
+#     plt.subplot(1, 4, 3)
+#     plt.plot(histr,color = col)
+#     plt.xlim([0,256])
+# plt.title('sadia normal')
+#
+# img_to_yuv = cv2.cvtColor(img, cv2.COLOR_BGR2YUV)
+# img_to_yuv[:, :, 0] = cv2.equalizeHist(img_to_yuv[:, :, 0])
+# hist_equalization_result = cv2.cvtColor(img_to_yuv, cv2.COLOR_YUV2BGR)
+#
+# color = ('b','g','r')
+# for channel,col in enumerate(color):
+#     histr = cv2.calcHist([hist_equalization_result],[channel],None,[256],[0,256])
+#     plt.subplot(1, 4, 4)
+#     plt.plot(histr,color = col)
+#     plt.xlim([0,256])
+# plt.title('sadia equal')
+#
+# plt.show()
