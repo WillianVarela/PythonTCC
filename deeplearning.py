13b92752c73c5824ee807da9eef5238b15d1f4fa
WillianNightray <willian-varela@hotmail.com>
2020-11-20 19:03:05 -0300

    teste

--- deeplearning.py
+++ deeplearning.py
@@ -15,6 +15,7 @@ IMG_WIDTH = 64
 EPOCHS = 50
 train_dir = pathlib.Path('train/')
 test_dir = pathlib.Path('test/')
+test_dir2 = pathlib.Path('teste2/')
 CLASS_NAMES = np.array([item.name for item in train_dir.glob('*')])
 tf.random.set_seed(1)
 
@@ -156,8 +157,65 @@ class DeepLearning(object):
         open("converted_model.tflite", "wb").write(tflite_model)
         pass
 
+    def plot_image(self, i, predictions_array, true_label, img):
+        predictions_array, true_label, img = predictions_array[i], true_label[i], img[i]
+        plt.grid(False)
+        plt.xticks([])
+        plt.yticks([])
+
+        plt.imshow(np.array(img, np.int32))
+
+        predicted_label = np.argmax(predictions_array)
+        if predicted_label == np.argmax(true_label):
+            color = 'green'
+        else:
+            color = 'red'
+
+        plt.xlabel("{} {:2.0f}% ({})".format(CLASS_NAMES[predicted_label],
+                                             100 * np.max(predictions_array),
+                                             CLASS_NAMES[np.argmax(true_label)]),
+                   color=color)
+
+    def plot_value_array(self, i, predictions_array, true_label):
+        predictions_array, true_label = predictions_array[i], true_label[i]
+        plt.grid(False)
+        tick_marks = np.arange(len(CLASS_NAMES))
+        plt.xticks(tick_marks, CLASS_NAMES)
+        plt.yticks([])
+        thisplot = plt.bar(range(2), predictions_array, color="#777777")
+        plt.ylim([0, 1])
+        predicted_label = np.argmax(predictions_array)
+
+        thisplot[predicted_label].set_color('red')
+        thisplot[np.argmax(true_label)].set_color('green')
+
+    def gerar_dados(self):
+        model = tf.keras.models.load_model('model.h5')
+
+        teste_data_gen = ImageDataGenerator().flow_from_directory(directory=str(test_dir2), batch_size=32,
+                                                                  target_size=(
+                                                                      64, 64),
+                                                                  class_mode="categorical",
+                                                                  shuffle=False
+                                                                  )
+        test_data, test_labels = next(teste_data_gen)
+
+        predictions = model.predict(test_data)
+        num_rows = 4
+        num_cols = 4
+        num_images = num_rows * num_cols
+        plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+        for i in range(num_images):
+            plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+            self.plot_image(i, predictions, test_labels, test_data)
+            plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+            self.plot_value_array(i, predictions, test_labels)
+        plt.show()
+
+
 ia = DeepLearning()
-ia.training_IA()
+#ia.training_IA()
+ia.gerar_dados()
 # ia.download_tflite()
 # test_data = test_data - np.mean(test_data, axis=0)  # zero-centering
 # test_data = test_data / np.std(test_data, axis=0)  # normalization
